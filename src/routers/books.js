56a191cadd3ec19e2440735d193d23d052928845
Kiran Gurung <kg820532@gmail.com>
2022-03-29 12:44:21 +0100

    put and delete

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,7 +3,29 @@ const booksRouter = express.Router()
 const db = require('../utils/database')
 
 booksRouter.get('/', (req, res) => {
-    db.query(' SELECT * FROM books')
+
+    let selectBookSQL = ' SELECT * FROM books'
+    const selectValues = []
+    const queries = []
+
+    if (req.query.type) {
+        queries.push({ col: 'type', val: req.query.type })
+    }
+
+    if (req.query.topic) {
+        queries.push({ col: 'topic', val: req.query.topic })
+    }
+    if (queries.length > 0) {
+        let whereClauses = []
+        queries.forEach((query, index) => {
+            whereClauses.push(`${query.col} = $${index + 1}`)
+            selectValues.push(query.val)
+        })
+        selectBookSQL += ' WHERE ' + whereClauses.join(' AND ')
+    }
+    console.log(selectValues)
+  console.log(selectBookSQL)
+    db.query(selectBookSQL, selectValues)
         .then(dbResult => {
             res.json({ books: dbResult.rows })
         })
@@ -55,8 +77,6 @@ booksRouter.post('/', (req, res) => {
         req.body.publicationDate,//$5
         req.body.pages //$6
     ]
-    console.log(req.body)
-    console.log(bookValues)
     db.query(insertBookSQL, bookValues)
         .then(databaseResult => {
             console.log(databaseResult)
@@ -68,5 +88,71 @@ booksRouter.post('/', (req, res) => {
             res.json({ error: 'unexpected error' })
         })
 })
+
+
+
+booksRouter.put('/:bookId', (req, res) => {
+    const updateBookSQL = `
+    UPDATE books SET 
+      title = $1,
+      type = $2,
+      author = $3,
+      topic = $4,
+      publicationDate = $5,
+      pages = $6
+    WHERE id = $7
+    RETURNING *`
+
+    const updateValues = [
+        req.body.title, //$1 = title
+        req.body.type, //$2 = type
+        req.body.author, //$3 = author
+        req.body.topic, //$4 = topic
+        req.body.publicationDate, //$5 = publicationDate
+        req.body.pages, //$6 = pages
+        req.params.bookId, //$7 = bookId
+    ]
+
+    db.query(updateBookSQL, updateValues)
+        .then(databaseResult => {
+            console.log(databaseResult)
+            if (databaseResult.rowCount === 0) {
+                res.status(404)
+                res.json({ error: 'book does not exist' })
+            }
+            else {
+                res.json({ book: databaseResult.rows[0] })
+            }
+        })
+        .catch(e => {
+            console.log(e)
+            res.status(500)
+            res.json({ error: 'unexpected error' })
+        })
+
+
+})
+
+
+
+
+booksRouter.delete('/:id', (req, res) => {
+    const deletebookSQL = `DELETE FROM books WHERE id = $1 RETURNING *`
+
+    const deleteId = [req.params.id]
+    db.query(deletebookSQL, deleteId)
+        .then(dbResult => {
+            if (dbResult.rowCount === 0) {
+                res.status(404)
+                res.json({ error: 'book does not exist' })
+            }
+            else { res.json({ book: dbResult.rows[0] }) }
+        })
+        .catch(error => {
+            res.status(500)
+            res.json({ error: 'unexpected error' })
+        })
+})
+
 
 module.exports = booksRouter
--- src/routers/pets.js
+++ src/routers/pets.js
@@ -57,6 +57,60 @@ petsRouter.post('/', (req,res) => {
 
 })
 
+petsRouter.put('/:id', (req, res) => {
+const updatePetSQL = `
+UPDATE pets SET
+name = $1,
+age = $2,
+type = $3,
+breed = $4,
+microchip = $5
+WHERE id = $6
+RETURNING *
+`
 
+const petValues = [
+    req.body.name,
+    req.body.age,
+    req.body.type,
+    req.body.breed,
+    req.body.microchip,
+    req.params.id
+]
+db.query(updatePetSQL, petValues)
+.then(dbResult => {
+    if(dbResult.rowCount === 0){
+        res.status(404)
+        res.json({error: 'pet not found'})
+    }else {
+        res.json({pet: dbResult})
+    }
+})
+.catch(e => {
+    res.status(500)
+    res.json({error : 'unexpected error'})
+})
+
+})
+
+
+petsRouter.delete('/:id', (req, res) => {
+    const deletePetSql = 'DELETE FROM pets WHERE id = $1 RETURNING *'
+    const deleteValue = [req.params.id]
+
+    db.query(deletePetSql, deleteValue)
+    .then(dbResult => {
+        if(dbResult.rowCount === 0){
+            res.status(404)
+            res.json({ error : 'Pet doesnt exist'})
+        } else {
+            res.json({pet: dbResult.rows[0]})
+        }
+    })
+    .catch(e => {
+        res.status(500)
+        res.json({ error : 'unexpected error'})
+    })
+})
 
 module.exports = petsRouter
